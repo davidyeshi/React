162c4f1791114e4dc8b9a14d10aa604c5f2a4709
David Yeshi <davidyeshi0@gmail.com>
2020-05-23 23:58:34 +1000

    Using state in App.js

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -4,13 +4,24 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  // If state changes then REACT re-renders
+  state = {
+    persons: [
+      {name: 'David', age:24},
+      {name: 'Mike', age: 21},
+      {name: 'Robert', age: 20}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>React App</h1>
-        <Person name = "David" age = "24" />
-        <Person name = "Mike" age = "21"> My hobbies: Racing</Person>
-        <Person name = "Robert" age = "20"/>
+        <button>Switch Name</button>
+        <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age} />
+        <Person name = {this.state.persons[1].name}  age = {this.state.persons[1].age}> My hobbies: Racing</Person>
+        <Person name = {this.state.persons[2].name}  age = {this.state.persons[2].age} />
       </div>
     );
   }
