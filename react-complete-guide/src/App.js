0224884c8985239ebb8a19724c2ebbda71716129
David Yeshi <davidyeshi0@gmail.com>
2020-05-24 00:09:13 +1000

    Manipulating state using method

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -14,11 +14,21 @@ class App extends Component {
     ]
   }
 
+  // method for switching name as an event handler on button click
+  switchNameHandler = () => {
+    console.log('Was Clicked!');
+    this.setState({persons: [
+      {name: 'Yeshi', age:24},
+      {name: 'Mike', age: 21},
+      {name: 'Robert', age: 20}
+    ]});
+  }
+
   render() {
     return (
       <div className="App">
         <h1>React App</h1>
-        <button>Switch Name</button>
+        <button onClick = {this.switchNameHandler}>Switch Name</button>
         <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age} />
         <Person name = {this.state.persons[1].name}  age = {this.state.persons[1].age}> My hobbies: Racing</Person>
         <Person name = {this.state.persons[2].name}  age = {this.state.persons[2].age} />
