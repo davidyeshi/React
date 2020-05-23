d5974d5fbe22d8f1e18fa35ce0c252765eb3b139
David Yeshi <davidyeshi0@gmail.com>
2020-05-24 01:00:45 +1000

    Styling with stylesheets

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import './Person.css'; // Thanks to webpack we import css without merging the two files.
+
 // Functional React Component
 // props contains all the properties of the component
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
