facfa05afb2941cb6bfa85b7cf75c0827b1907cb
David Yeshi <davidyeshi0@gmail.com>
2020-05-24 04:51:44 +1000

    Render dynamically

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -11,17 +11,21 @@ class App extends Component {
       {name: 'David', age:24},
       {name: 'Mike', age: 21},
       {name: 'Robert', age: 20}
-    ]
+    ],
+    showPersons: false
   }
 
   // method for switching name as an event handler on button click
   switchNameHandler = (newName) => {
     console.log('Was Clicked!');
-    this.setState({persons: [
-      {name: newName, age:24},
-      {name: 'Mike', age: 21},
-      {name: 'Robert', age: 20}
-    ]});
+    this.setState(
+      {
+        persons: [
+          {name: newName, age:24},
+          {name: 'Mike', age: 21},
+          {name: 'Robert', age: 20}
+        ]
+       });
   }
 
   // Change name handler
@@ -33,6 +37,12 @@ class App extends Component {
       {name: event.target.value, age: 21},
       {name: 'Robert', age: 20}
     ]});
+  }
+
+  togglePersonsHandler = () => {
+    // Render dynamically
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
   }
 
   render() {
@@ -52,19 +62,22 @@ class App extends Component {
         // inline style
         style = {style}
         // Alternative way of binding 
-        onClick = {() => this.switchNameHandler('Yeshi Minda!')}>Switch Name</button>
-        <Person 
-          name = {this.state.persons[0].name} 
-          age = {this.state.persons[0].age} />
-        <Person 
-          name = {this.state.persons[1].name}  
-          age = {this.state.persons[1].age}
-          // binding method between components (Recommended)
-          click = {this.switchNameHandler.bind(this, 'Minda')}
-          changed = {this.nameChangedHandler}> My hobbies: Racing</Person>
-        <Person 
-          name = {this.state.persons[2].name}  
-          age = {this.state.persons[2].age} />
+        onClick = {this.togglePersonsHandler}>Toggle Persons</button>
+
+        {this.state.showPersons ?<div>
+          <Person 
+            name = {this.state.persons[0].name} 
+            age = {this.state.persons[0].age} />
+          <Person 
+            name = {this.state.persons[1].name}  
+            age = {this.state.persons[1].age}
+            // binding method between components (Recommended)
+            click = {this.switchNameHandler.bind(this, 'Minda')}
+            changed = {this.nameChangedHandler}> My hobbies: Racing</Person>
+          <Person 
+            name = {this.state.persons[2].name}  
+            age = {this.state.persons[2].age} />
+        </div> : null}
       </div>
     );
   }
