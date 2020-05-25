0005c0e0bc2f9e8dd08b68436c3f56caa894adb2
David Yeshi <davidyeshi0@gmail.com>
2020-05-26 00:15:22 +1000

    Using react ref

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -9,12 +9,27 @@ import classes from './Person.css'; // Thanks to webpack we import css without m
 // props contains all the properties of the component
 class Person extends Component {
 
+    constructor(props) {
+        super(props);
+        // definging ref
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.inputElementRef.current.focus();
+    }
+
     render(){
         return (
             <Aux>
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name}/>
+                <input
+                    // ref={(inputEl) => {this.inputElement = inputEl}} 
+                    ref = {this.inputElementRef}
+                    type="text" 
+                    onChange={this.props.changed} 
+                    value={this.props.name}/>
             </Aux>
         );
     }
