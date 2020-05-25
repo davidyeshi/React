620add579acbb7332b13a645426fce8b07ed65b6
David Yeshi <davidyeshi0@gmail.com>
2020-05-25 23:23:46 +1000

    Rendering adjacent elements by using HOC as a wrapper

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import Aux from '../../../hoc/Aux';
 import classes from './Person.css'; // Thanks to webpack we import css without merging the two files.
 
 // Functional React Component
@@ -8,11 +8,11 @@ class Person extends Component {
 
     render(){
         return (
-            <div className={classes.Person}>
+            <Aux className={classes.Person}>
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name}/>
-            </div>
+            </Aux>
         );
     }
 }
