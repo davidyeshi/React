219464c0af406a851f33ea3a7709977aea5c90ee
David Yeshi <davidyeshi0@gmail.com>
2020-05-26 09:32:08 +1000

    Adding the layout component

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
     <div>
-      
+      <Layout>
+        <p>Page to be displayed</p>
+      </Layout>      
     </div>
   );
 }
