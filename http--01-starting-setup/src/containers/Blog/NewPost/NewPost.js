b8255c16e7d7007a8e892ee06cf8f6850c3e83bd
David Yeshi <davidyeshi0@gmail.com>
2020-05-28 11:35:56 +1000

    Redirect conditionaly in new-post

--- http--01-starting-setup/src/containers/Blog/NewPost/NewPost.js
+++ http--01-starting-setup/src/containers/Blog/NewPost/NewPost.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {Redirect} from 'react-router-dom';
 
 import './NewPost.css';
 
@@ -7,7 +8,8 @@ class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'Max',
+        submitted: false
     }
 
     componentDidMount() {
@@ -22,6 +24,7 @@ class NewPost extends Component {
         axios.post('/posts', post)
             .then(response => {
                 console.log(response);
+                this.setState({submitted: true})
             })
             .catch(error => {
                 console.log('postHandler failed');
@@ -29,8 +32,12 @@ class NewPost extends Component {
             });
     };
     render () {
+        let redirect = null;
+        if (this.state.submitted) {redirect = <Redirect to = "/"/>; }
+        
         return (
             <div className="NewPost">
+                {redirect}
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
