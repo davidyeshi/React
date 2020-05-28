51a64fa093652e16a2435c9eeede87451006c705
David Yeshi <davidyeshi0@gmail.com>
2020-05-28 14:38:46 +1000

    Lazy Loading

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+// import NewPost from './NewPost/NewPost';
 import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
+import asyncComponent from '../../hoc/asyncComponet';
 
 // import axios from 'axios';
 import './Blog.css';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
 
@@ -36,10 +41,10 @@ class Blog extends Component {
 
                 {/* Loads only one route  */}
                 <Switch>
-                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/new-post" component={AsyncNewPost} />
                     <Route path="/posts" component={Posts} />
-                    <Route render={()=> <h1>Not found</h1>} />
-                    {/* <Redirect from="/" to="/posts" /> */}
+                    {/* <Route render={()=> <h1>Not found</h1>} /> */}
+                    <Redirect from="/" to="/posts" />
                 </Switch>
             </div>
         );
