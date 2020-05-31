a5c8db7f4f89290101c5da32a7e9eae75dbefee8
David Yeshi <davidyeshi0@gmail.com>
2020-05-31 12:23:48 +1000

    Handling form submission

--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -70,11 +70,15 @@ class ContactData extends Component {
         event.preventDefault();
 
         this.setState({loading: true});
+        const formData = {};
+        for (let formElementIdentifier in this.state.orderForm) {
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        }
 
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.totalPrice,
-            
+            orderData: formData
         };
 
         axios.post('/orders.json', order)
@@ -113,7 +117,7 @@ class ContactData extends Component {
         }
 
         let form = (
-            <form>
+            <form onSubmit={this.orderHandler}>
                     {formElementsArray.map(formElement => {
                         return <Input 
                             key={formElement.id}
@@ -122,7 +126,7 @@ class ContactData extends Component {
                             value={formElement.config.value}
                             changed={(event) => this.inputChangedHandler(event,formElement.id)}/>
                     })}
-                    <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+                    <Button btnType="Success">ORDER</Button>
             </form>
         );
         if (this.state.loading) {
