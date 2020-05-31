c306d0be2f308e73ee80f4e40aa16f6e407a0462
David Yeshi <davidyeshi0@gmail.com>
2020-05-31 12:16:20 +1000

    Handling User Input

--- burger-builder/src/components/UI/Input/Input.js
+++ burger-builder/src/components/UI/Input/Input.js
@@ -7,23 +7,36 @@ const input = (props) => {
 
     switch(props.elementType) {
         case ('input'):
-            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value}/>;
+            inputElement = <input 
+                className={classes.InputElement} 
+                onChange={props.changed}
+                {...props.elementConfig} 
+                value={props.value}/>;
             break;
         case ('textarea') :
-            inputElement = <textarea className={classes.InputElement} {...props.elementConfig} value={props.value}/>;
+            inputElement = <textarea 
+                className={classes.InputElement} 
+                onChange={props.changed}
+                {...props.elementConfig} 
+                value={props.value}/>;
             break;
         case ('select'):
             inputElement = (
             <select
                 className={classes.InputElement} 
-                value={props.value}>
+                value={props.value}
+                onChange={props.changed}>
                 {props.elementConfig.options.map(option=>(
                     <option key={option.value} value={option.value}>{option.displayValue}</option>
                 ))}
             </select>);
             break;
         default:
-            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value}/>
+            inputElement = <input 
+                className={classes.InputElement} 
+                onChange={props.changed}
+                {...props.elementConfig} 
+                value={props.value}/>
     }
 
     return(
