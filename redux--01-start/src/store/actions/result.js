229130e5fd11396b5ac47bd945b5bc0f6ae1633f
David Yeshi <davidyeshi0@gmail.com>
2020-06-08 16:50:41 +1000

    Restructuring actions

---
+++ redux--01-start/src/store/actions/result.js
@@ -0,0 +1,25 @@
+import * as actionTypes from './actionsTypes';
+
+
+export const saveResult = (result) => {
+    return {
+        type: actionTypes.STORE_RESULT,
+        result: result
+    };
+}
+
+export const storeResult = (result) => {
+    // async with redux thunk
+    return dispatch => {
+        setTimeout(()=> {
+            dispatch(saveResult(result))
+        },2000);
+    }
+};
+
+export const deleteResult = (id) => {
+    return {
+        type: actionTypes.DELETE_RESULT,
+        resultId: id
+    };
+};
