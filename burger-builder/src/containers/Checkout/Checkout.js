5a6c4359311dc5bd3d83907e2a79dcfdbd732361
David Yeshi <davidyeshi0@gmail.com>
2020-06-09 15:50:51 +1000

    Combining Reducers

--- burger-builder/src/containers/Checkout/Checkout.js
+++ burger-builder/src/containers/Checkout/Checkout.js
@@ -54,7 +54,7 @@ class Checkout extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.ingredients
+        ings: state.burgerBuilder.ingredients
     }
 }
 export default connect(mapStateToProps)(Checkout);
