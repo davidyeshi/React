8b800a22e8d696cf3117ad7483a3161bb639dde0
David Yeshi <davidyeshi0@gmail.com>
2020-06-13 15:09:18 +1000

    Signing up user and getting token from firebase

--- burger-builder/src/store/actions/auth.js
+++ burger-builder/src/store/actions/auth.js
@@ -1,3 +1,6 @@
+import axios from 'axios';
+
+
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -23,5 +26,21 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart());
+        
+        const authData = {
+            email: 'xyz@gmail.com',
+            password: 'password',
+            returnSecureToken: true
+        }
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCn2m8-ylbVFFmy7BZe89kVH3QX9j0uKVk', authData)
+            .then(res => {
+                console.log(res);
+                dispatch(authSuccess(res.data));
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err));
+            })
     };
 };
+
