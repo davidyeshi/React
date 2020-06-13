eae40a45b26d17fce132915c4a40e0b48e76cadd
David Yeshi <davidyeshi0@gmail.com>
2020-06-13 21:53:07 +1000

    Guarding routes

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -4,7 +4,7 @@ import {connect} from 'react-redux';
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
@@ -16,19 +16,40 @@ class App extends Component {
   }
 
   render() {
+
+    let routes = (
+      <Switch>
+        <Route path="/auth" component = {Auth} />
+        <Route path="/" exact component = {BurgerBuilder} />
+        <Redirect to="/" />
+      </Switch>
+    );
+
+    if(this.props.isAuth) {
+      routes = (
+        <Switch>
+          <Route path="/checkout" component = {Checkout} />
+          <Route path="/orders" component = {Orders} />
+          <Route path="/logout" component = {Logout} />
+          <Route path="/" exact component = {BurgerBuilder} />
+          <Redirect to="/" />
+        </Switch>
+      )
+    }
+
     return (
       <div>
         <Layout>
-          <Switch>
-            <Route path="/checkout" component = {Checkout} />
-            <Route path="/orders" component = {Orders} />
-            <Route path="/auth" component = {Auth} />
-            <Route path="/logout" component = {Logout} />
-            <Route path="/" exact component = {BurgerBuilder} />
-          </Switch>
+          {routes}
         </Layout>      
       </div>
     );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    isAuth: state.auth.token !== null
   }
 }
 
@@ -38,4 +59,4 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
